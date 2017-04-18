89743dae875eb939a7206d41dcbfc425a6a77483
wanghuan <29479336@qq.com>
2017-04-18 12:49:02 +0800

    更改方法

---
+++ WebApplication1/WebApplication1/Controllers/BlogController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            ViewBag.Message = "My first Page.";
+            return View();
+        }
+        public ActionResult Say()
+        {
+            return View();
+        }
+    }
+}
